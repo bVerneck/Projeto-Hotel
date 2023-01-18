5f6982595bef78b056b92e64b83f69253f22989b
Cristian Marcucci <cristian.marcuci@gmail.com>
2023-01-17 21:18:36 -0300

    ajustes no código - retirada de elementos esquecidos

--- js/reservas-public.js
+++ js/reservas-public.js
@@ -1,6 +1,13 @@
+const teste = document.querySelector("#entradaCheckIn");
 
+function calendario() {
+  console.log("works");
+  let apresentacao = document.createElement("input");
+  teste.appendChild(apresentacao);
+}
+
+teste.addEventListener("click", calendario);
 
 import modal from "/js/modules/modal-reservas-public.js";
 
-modal()
-
+modal();
