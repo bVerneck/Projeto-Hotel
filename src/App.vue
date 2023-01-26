17acee6e387c5cab14cc2abaf90cad19288a6e35
bVerneck <bruno5966verneck@gmail.com>
2023-01-26 14:52:27 -0300

    iniciando a componentização header unificado

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <unifiedHeader />
+    
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import unifiedHeader from '../src/components/unifiedHeader.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    unifiedHeader
+  },
+  
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
