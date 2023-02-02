c14dd8eabcfdd34f2a948fb2b5a4ec9fd934a2bb
Renor Oliveira <renornew@gmail.com>
2023-02-02 20:39:22 -0300

    add formulário na página de login public

--- src/views/LoginPublic.vue
+++ src/views/LoginPublic.vue
@@ -1,14 +1,144 @@
 <template>
-    <UnifiedHeader />
-    <p>Login</p>
-    <UnifiedFooter />
+    
+   <UnifiedHeader />  
+    <main>
+        <div id="login-container">
+      <h1>Login</h1>
+      <form action="">
+        <label for="email">E-mail</label>
+        <input type="email" name="login" id="login" placeholder="Insira seu e-mail" autocomplete="off">
+        <label for="password">Senha</label>
+        <input type="password" name="password" id="password" placeholder="Insira sua senha">
+        <input type="submit" onclick="logar(); return false" value="Confirmar">
+        <a href="#" id="esqueci_senha">Esqueceu sua senha?</a>
+      </form>
+      <div id="social-container">
+        <p>Ou entre pelas suas redes sociais</p>
+        <ul>
+        <li>
+          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'facebook' }"/>
+          <span>Facebook</span>
+        </li>
+        <li>
+          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'instagram' }"/>
+          <span>Instagram</span>
+        </li>
+        <li>
+          <font-awesome-icon :icon="{ prefix: 'fab', iconName: 'twitter' }"/>
+          <span>Twitter</span>
+        </li>
+      </ul>
+      </div>
+      <div id="registrar_container">
+        <p>Ainda não tem uma conta?</p>
+        <a href="../pages/cadastro.html">Registrar</a>
+      </div>
+    </div>
+    </main> 
+     <br />
+   <br />
+   <br /> 
+   <UnifiedFooter />  
+   
 </template>
 
 <script>
 import UnifiedHeader from '@/components/UnifiedHeader.vue';
 import UnifiedFooter from '@/components/UnifiedFooter.vue';
+
 export default{
-    name: "login-public",
-    components: {UnifiedHeader, UnifiedFooter}
-}
+    name: "loginPublic",
+    components: {
+        UnifiedHeader,         
+        UnifiedFooter
+    },
+};
 </script>
+
+<style>
+
+
+form { 
+    margin-top: 30px;
+    margin-bottom: 40px;
+}
+
+label, input {
+  display: block;
+  width: 100%;
+  text-align: left; }
+
+label {
+  font-weight: bold;
+  font-size: 0.8rem; }
+
+  input {
+  border-bottom: 2px solid #131212;
+  padding: 10px;
+  font-size: 1rem;
+  margin-bottom: 20px; }
+
+  input[type="submit"] {
+  text-align: center;
+  text-transform: uppercase;
+  font-weight: bold;
+  border: none;
+  height: 40px;
+  border-radius: 20px;
+  margin-top: 20px;
+  color: #fff;
+  background-color: #0dadbd;
+  cursor: pointer; }
+
+  input[type="submit"]:hover {
+  background-color: #afdaa7;
+  transition: 0.5s; }
+
+  #login-container {
+    background-color: rgb(235, 243, 235);
+  width: 400px;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px 30px;
+  margin-top: 10vh;
+  border-radius: 10px;
+  text-align: center;
+  }
+
+  /* redes sociais*/
+#social-container, #social-container p {
+  margin-bottom: 20px; }
+
+#social-container i {
+  height: 40px;
+  width: 40px;
+  border-radius: 50%;
+  line-height: 40px;
+  color: #fff;
+  margin: 0 5px;
+  cursor: pointer; }
+
+.fa-facebook {
+  background-color: #3b5998; }
+
+.fa-linkedin {
+  background-color: #0077b5; }
+
+.fa-twitter {
+  background-color: #00acee; }
+
+/* registrar */
+#registrar_container {
+  margin-bottom: 10px; }
+
+  ul {
+    display: flex;
+    padding: 0 50px;
+    justify-content: space-between;
+  }
+
+  ul, li {
+    padding: 0 10px;
+  }
+
+</style>
