533098fe2fabad33132d4967bb74179128e47f0c
Raylan Medeiros <raylanmedeiros.rm@gmail.com>
2023-04-17 15:50:01 -0300

    novos ajustes

--- JS/unified-header.js
+++ JS/unified-header.js
@@ -1,5 +1,15 @@
+let prevScrollpos = window.pageYOffset;
+let timeoutId = setTimeout(() => {
+    document.querySelector('.header').style.transition = 'top 0.4s ease-in-out';
+}, 300);
 
+window.addEventListener('scroll', () => {
+    const currentScrollPos = window.pageYOffset;
 
-function open(){
-    alert("você clicou!");
-}
+    if (prevScrollpos > currentScrollPos) {
+        document.querySelector('.header').style.top = '0';
+    } else {
+        document.querySelector('.header').style.top = '-100px';
+    }
+    prevScrollpos = currentScrollPos;
+});
